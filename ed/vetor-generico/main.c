6b0604cded4d531224cd99d290f6a35d75372780
edualfo <eduardo.abreulfo@gmail.com>
2023-08-18 17:11:00 -0300

    testando

--- ed/vetor-generico/main.c
+++ ed/vetor-generico/main.c
@@ -7,9 +7,11 @@ int main()
 {
     Vector *v = vector_create();
 
-    VectorItem *item = vector_item_create();
-
-    
+    for(int i=0; i<15; i++)
+    {
+        VectorItem *item = vector_item_create();
+        vector_add(v, item);
+    }
 
     vector_destroy(v);
 
